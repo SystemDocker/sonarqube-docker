86cd56bcfea34c398e7b4eb88873e8f51bfd399c
Fabrizio Lungo <git@flungo.me>
2017-02-07 14:34:43 +0000

    Removed all variants except 6.2-alpine

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-docker build -t sonarsource/sonar-5.6.5 5.6.5
-docker build -t sonarsource/sonar-5.6.5-alpine 5.6.5-alpine
-docker build -t sonarsource/sonar-6.2 6.2
-docker build -t sonarsource/sonar-6.2-alpine 6.2-alpine
